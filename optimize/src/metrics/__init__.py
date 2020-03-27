f44eb5a198d5316d5938522d212af95210e30cca
Joel Simon <joelsimon6@gmail.com>
2020-03-27 14:53:26 -0700

    make all comapctness errors be error-squared

--- optimize/src/metrics/__init__.py
+++ optimize/src/metrics/__init__.py
@@ -1,3 +1,3 @@
-from .fairness import efficiency_gap, dem_advantage, rep_advantage
-from .compactness import reock, convex_hull, polsby_popper, center_distance
+from .fairness import efficiency_gap, dem_advantage, rep_advantage, lost_votes 
+from .compactness import reock, convex_hull, polsby_popper, center_distance, bounding_hulls
 from .equality import equality
--- optimize/src/metrics/compactness.pyx
+++ optimize/src/metrics/compactness.pyx
@@ -14,37 +14,45 @@ from src.districts import district_boundry_points
 
 ################################################################################
 # Utils.
-cdef inline float udist(float[:] a, float[:] b):
-    """ Euclidian distance between 2 2D vectors. Faster than np.linalg.norm. """
+
+cdef inline float udist_sq(float[:] a, float[:] b):
     cdef float x = a[0] - b[0]
     cdef float y = a[1] - b[1]
-    return sqrt(x*x + y*y)
+    return x*x + y*y
+
+cdef inline float udist(float[:] a, float[:] b):
+    """ Euclidian distance between 2 2D vectors. Faster than np.linalg.norm. """
+    return sqrt(udist_sq(a, b))
+
 ################################################################################
 
 cpdef float center_distance(state, int[:] districts, int n_districts) except *:
+    """ Returns the mean distance squared from each tile to its respective
+        district center. Normalized to [0, 1] """
     cdef float[:,:] tile_centers = state.tile_centers
     cdef float[:,:] dist_centers = np.zeros((n_districts, 2), dtype='float32')
-    cdef float[:] distances = np.zeros(n_districts, dtype='float32')
-    cdef float[:] sizes = np.zeros(n_districts, dtype='float32')
+    cdef float[:] dist_sizes = np.zeros(n_districts, dtype='float32')
     cdef int ti, di
+    cdef int n_tiles = state.n_tiles
 
     # Find the average point.
-    for ti in range(state.n_tiles):
+    for ti in range(n_tiles):
         di = districts[ti]
         dist_centers[di, 0] += tile_centers[ti, 0]
         dist_centers[di, 1] += tile_centers[ti, 1]
-        sizes[di] += 1
-
+        dist_sizes[di] += 1
     for di in range(n_districts):
-        dist_centers[di, 0] /= sizes[di]
-        dist_centers[di, 1] /= sizes[di]
+        dist_centers[di, 0] /= dist_sizes[di]
+        dist_centers[di, 1] /= dist_sizes[di]
 
-    # Find distances to average point.
-    for ti in range(state.n_tiles):
+    # Find average distance to dsitrict center.
+    cdef float max_dist = fmax(state.bbox[2]-state.bbox[0], state.bbox[3]-state.bbox[1])
+    cdef float mean_dist_sq = 0.0 
+    for ti in range(n_tiles):
         di = districts[ti]
-        distances[di] += udist(tile_centers[ti], dist_centers[di]) / sizes[di]
-
-    return np.mean(distances) / max( state.bbox[2]-state.bbox[0], state.bbox[3]-state.bbox[1] )
+        mean_dist_sq += udist_sq(tile_centers[ti], dist_centers[di]) / n_tiles
+        
+    return mean_dist_sq / (max_dist * max_dist)
 
 ################################################################################
 
@@ -66,13 +74,16 @@ cpdef float polsby_popper(state, int[:] districts, int n_districts) except *:
 
     cdef float pp_score = 0
     for di in range(n_districts):
-        pp_score += ((4 * pi * dist_areas[di]) / (dist_perimeters[di]*dist_perimeters[di]))
+        pp_score += sqrt((4 * pi * dist_areas[di]) / (dist_perimeters[di]*dist_perimeters[di]))
     
     pp_score /= n_districts
 
     return 1.0 - pp_score
 
 ################################################################################
+
+def sum_sq(iter):
+    return sum(v * v for v in iter)
 
 def bounding_hulls(state, districts, n_districts):
     """ Return the convex hull of each district. """
@@ -82,8 +93,8 @@ def bounding_hulls(state, districts, n_districts):
 def convex_hull(state, districts, n_districts):
     """ Compactness metric: inverse to the sum hull areas. """
     hulls = bounding_hulls(state, districts, n_districts)
-    hulls_area = sum( polygon.area([ h ]) for h in hulls )
-    return 1.0 - (state.area / hulls_area)
+    hulls_area = sum_sq( polygon.area([ h ]) for h in hulls )
+    return 1.0 - (state.area * state.area / hulls_area)
 
 ################################################################################
 
@@ -95,5 +106,5 @@ def bounding_circles(state, districts, n_districts):
 def reock(state, districts, n_districts):
     """ Compactness metric: inverse to the sum circle areas. """
     circles = bounding_circles(state, districts, n_districts)
-    circles_area = sum( pi * c[2]**2 for c in circles )
-    return 1.0 - ( state.area / circles_area )
+    circles_area = sum_sq( pi * c[2]**2 for c in circles )
+    return 1.0 - ( state.area*state.area / circles_area )
